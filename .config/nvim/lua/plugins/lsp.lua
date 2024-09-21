317fe7746f81ae8ba4c85bc4403b516dd7f20cf2
No0ne003 <hi.yassir.op@gmail.com>
2024-09-21 14:37:48 +0100

    feat: formatting and lenting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,45 +1,51 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+	{
+		"williamboman/mason.nvim",
+		cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
 
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "eslint", -- eslint-lsp
-          "ts_ls", -- typescript-language-server
-          "tailwindcss", -- tailwindcss-language-server
-          "html", -- html-lsp
-          "cssls", -- css-lsp
-          "rust_analyzer", -- rust-analyzer
-          "lua_ls", -- lua-language-server
-          "clangd", -- clangd
-          "bashls", -- Bash LSP
-          "jsonls", -- JSON LSP for better support with JSON files
-          "yaml_ls", -- YAML LSP for YAML files
-          "marksman", -- Markdown LSP for better markdown support
-        }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+		opts = {
+			ensure_installed = {
+				"biome",
+        "eslint_d"
+			},
+		},
 
-      -- Keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP definition" })
-      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Action" })
-    end
-  }
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"eslint", -- eslint-lsp
+					"ts_ls", -- typescript-language-server
+					"tailwindcss", -- tailwindcss-language-server
+					"html", -- html-lsp
+					"cssls", -- css-lsp
+					"rust_analyzer", -- rust-analyzer
+					"lua_ls", -- lua-language-server
+					"clangd", -- clangd
+					"bashls", -- Bash LSP
+					"jsonls", -- JSON LSP for better support with JSON files
+					"yaml_ls", -- YAML LSP for YAML files
+					"marksman", -- Markdown LSP for better markdown support
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+
+			-- Keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP hover" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP definition" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Action" })
+		end,
+	},
 }
-
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "c", "javascript"},
+      auto_install = true,
       highlight = {
         enable = true,
       },
