9917bee80a55100e2305f5d35e977d721f86a803
No0ne003 <hi.yassir.op@gmail.com>
2024-11-23 09:36:15 +0100

    feat: render-markdown.nvim on mdx files and syntax highlighting for mdx files also

--- .config/nvim/lua/plugins/configs/treesitter.lua
+++ .config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,8 +1,13 @@
 local config = require("nvim-treesitter.configs")
 config.setup({
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = true }
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = { enable = true },
 })
+
+vim.filetype.add({
+	extension = { mdx = "mdx" },
+})
+vim.treesitter.language.register("markdown", "mdx")
