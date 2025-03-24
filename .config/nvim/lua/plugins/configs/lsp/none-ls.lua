2b2ebc32a1ef4c8b8a14f468b850707f4c21f554
nuexq <hi.yassir.op@gmail.com>
2025-03-24 16:59:57 +0000

    biome for diagnostics also

--- .config/nvim/lua/plugins/configs/lsp/none-ls.lua
+++ .config/nvim/lua/plugins/configs/lsp/none-ls.lua
@@ -21,6 +21,7 @@ null_ls.setup({
 			filetypes = { "astro" },
 		}),
 		null_ls.builtins.diagnostics.eslint_d, -- Use the built-in diagnostics for eslint_d
+		null_ls.builtins.diagnostics.biome,
 		null_ls.builtins.formatting.eslint_d,
 	},
 })
